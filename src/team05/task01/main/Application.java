2650b90a2cfdca6b6900e51488af5805eb5bab80
philippwi <philippwi95@gmail.com>
2017-02-22 12:57:19 +0100

    Initial commit

---
+++ src/team05/task01/main/Application.java
@@ -0,0 +1,7 @@
+package taskgroupXY.taskXY.main;
+
+public class Application {
+    public static void main(String... args) {
+        System.out.println("hello world"); // demo
+    }
+}
---
+++ src/team05/task13/main/Application.java
@@ -0,0 +1,7 @@
+package taskgroupXY.taskXZ.main;
+
+public class Application {
+    public static void main(String... args) {
+        System.out.println("hello world"); // demo
+    }
+}
